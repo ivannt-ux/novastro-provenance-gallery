6d91afc49195910cf29d2702a621888de5dc5bb6
ivannt-ux <onyemaebuka3@gmail.com>
2025-08-06 00:50:19 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="text-center space-y-4">
+      <h1 className="text-3xl font-bold">Novastro Provenance Gallery</h1>
+      <p>
+        Represent real-world assets in evolving, programmable, on-chain formats.
+      </p>
+    </div>
+  );
+}
