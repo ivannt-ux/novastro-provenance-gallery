35b2bf8cb291829d8e261169ea253f2bc50ac7b8
ivannt-ux <onyemaebuka3@gmail.com>
2025-08-06 01:36:00 +0100

    Update page.tsx

--- app/assets/[id]/milestone/page.tsx
+++ app/assets/[id]/milestone/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { addMilestone } from "@/lib/data";
+
+import { addMilestone } from "../../../../lib/data";
 
 export default function AddMilestone({ params }: { params: { id: string } }) {
   const router = useRouter();
