e45784b5f5faf84b49a9be3ebb49e241740837fe
ivannt-ux <onyemaebuka3@gmail.com>
2025-08-08 02:40:35 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
-    <div className="text-center space-y-4">
-      <h1 className="text-3xl font-bold">Novastro Provenance Gallery</h1>
-      <p>
-        Represent real-world assets in evolving, programmable, on-chain formats.
-      </p>
-    </div>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="card text-center space-y-4 max-w-xl mx-auto">
+        <h1 className="text-3xl font-bold">Novastro Provenance Gallery</h1>
+        <p>
+          Represent real-world assets in evolving, programmable, on-chain formats.
+        </p>
+      </div>
+    </main>
   );
 }
